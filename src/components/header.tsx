73c94b2d9a7408a05b6c30ed64f0438eb8895b06
Kenneth <kenneth.sulaiman@gmail.com>
2025-10-04 00:13:38 +0700

    update resume

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
             <Image src={assets.right_arrow_white} alt="" className="w-4" />
           </a>
           <a
-            href="/KENNETH ANGELO SULAIMAN - CV.pdf"
+            href="/Resume - Kenneth Angelo Sulaiman.pdf"
             download
             className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
           >
