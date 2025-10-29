2ac33e21e6a6f7c4369e92b25f69bf6acdf7ec99
Kenneth <kenneth.sulaiman@gmail.com>
2025-10-29 23:02:33 +0700

    fix resume

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -35,7 +35,7 @@
               <Image src={assets.right_arrow_white} alt="" className="w-4" />
             </a>
             <a
-              href="/Resume_Kenneth Sulaiman.pdf"
+              href="/Resume_Kenneth Angelo Sulaiman.pdf"
               download
               className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2"
             >
